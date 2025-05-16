7549a5b1d3e8e9c1910b4d3d0aab5f19ab72e66e
Senthil Kumar Rani Sekar <senthil.sekar@exeevo.com>
2025-05-16 14:22:02 +0530

    addedpocactions

---
+++ POCSolution/POC.Actions/CreateNewEntity.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace POC.Actions
+{
+    public class CreateNewEntity : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("EntitySchemaName")]
+        public InArgument<string> EntitySchemaName { get; set; }
+
+        [RequiredArgument]
+        [Input("EntityDisplayName")]
+        public InArgument<string> EntityDisplayName { get; set; }
+
+        [RequiredArgument]
+        [Input("EntityPluralName")]
+        public InArgument<string> EntityPluralName { get; set; }
+
+        [RequiredArgument]
+        [Input("SolutionUniqueName")]
+        public InArgument<string> SolutionUniqueName { get; set; }
+             
+        [Output("Response")]
+        public OutArgument<string> Response { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            string entitySchemaName = $"poc_{EntitySchemaName.Get(context)}";
+            string entityDisplayName = EntityDisplayName.Get(context);
+            string entityPluralName = EntityPluralName.Get(context);
+            string solutionUniqueName = SolutionUniqueName.Get(context);
+
+            ITracingService tracingService = context.GetExtension<ITracingService>();           
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+
+            CreateEntityRequest request = new CreateEntityRequest {
+                Entity = new EntityMetadata
+                {
+                    SchemaName = entitySchemaName,
+                    DisplayName = new Label(entityDisplayName, 1033),
+                    DisplayCollectionName = new Label(entityPluralName, 1033),
+                    Description = new Label("This entity is created by SolMate", 1033),
+                    OwnershipType = OwnershipTypes.UserOwned,
+                    IsActivity = false,
+                    IsMailMergeEnabled = new BooleanManagedProperty(true),
+                    IsDuplicateDetectionEnabled = new BooleanManagedProperty(true)
+
+                },
+                PrimaryAttribute = new StringAttributeMetadata
+                {
+                    SchemaName = "poc_name",
+                    RequiredLevel = new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.SystemRequired),
+                    MaxLength = 100,
+                    FormatName = StringFormatName.Text,
+                    DisplayName = new Label("Name", 1033),
+                    Description = new Label($"The primary column for the {entityDisplayName} table.", 1033)
+                },
+                SolutionUniqueName = solutionUniqueName
+            };
+
+            var response = (CreateEntityResponse)service.Execute(request);
+            tracingService.Trace($"Response: {response.EntityId.ToString()}");            
+            Response.Set(context, response.EntityId.ToString());
+            // throw new NotImplementedException();
+        }
+    }
+}
---
+++ POCSolution/POC.Actions/CreateNewField.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+
+namespace POC.Actions
+{
+    public class CreateNewField : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("FieldDisplayName")]
+        public InArgument<string> FieldDisplayName { get; set; }
+
+        [RequiredArgument]
+        [Input("FieldUniqueName")]
+        public InArgument<string> FieldUniqueName { get; set; }
+
+        [RequiredArgument]
+        [Input("FieldType")]
+        public InArgument<string> FieldType { get; set; }
+
+        [RequiredArgument]
+        [Input("IsRequired")]
+        public InArgument<string> IsRequired { get; set; }
+
+        [RequiredArgument]
+        [Input("SolutionUniqueName")]
+        public InArgument<string> SolutionUniqueName { get; set; }
+
+        [RequiredArgument]
+        [Input("EntityLogicalName")]
+        public InArgument<string> EntityLogicalName { get; set; }
+
+        [Output("Response")]
+        public OutArgument<string> Response { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {            
+            string fieldDisplayName = FieldDisplayName.Get(context);
+            string fieldUniqueName = FieldUniqueName.Get(context);
+            string fieldType = FieldType.Get(context);
+            string entityLogicalName = EntityLogicalName.Get(context);
+            string solutionUniqueName = SolutionUniqueName.Get(context);
+            string isRequired = IsRequired.Get(context);
+            int languageCode = 1033;
+            string prefix = "poc"; 
+
+            ITracingService tracingService = context.GetExtension<ITracingService>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+
+            var addedAttributes = new List<AttributeMetadata>();
+            var fieldRequiredLevel = (isRequired == "Yes") ? new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.SystemRequired) : new AttributeRequiredLevelManagedProperty(AttributeRequiredLevel.None);
+            
+            switch (fieldType)
+                {                
+                    case "boolean":
+                        BooleanAttributeMetadata boolAttribute = new BooleanAttributeMetadata()
+                        {                            
+                            SchemaName = $"{prefix}_{fieldUniqueName}",
+                            LogicalName = $"{prefix}_{fieldUniqueName}",
+                            DisplayName = new Label(fieldDisplayName, languageCode),
+                            RequiredLevel = fieldRequiredLevel,
+                            Description = new Label("This field is created by SolMate", languageCode),
+                            OptionSet = new BooleanOptionSetMetadata(
+                            new OptionMetadata(new Label("True", languageCode), 1),
+                            new OptionMetadata(new Label("False", languageCode), 0)
+                            )
+                        };
+                    addedAttributes.Add(boolAttribute);
+                    break;
+                case "datetime":
+                    DateTimeAttributeMetadata dtAttribute = new DateTimeAttributeMetadata()
+                    {                        
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        Format = Microsoft.Xrm.Sdk.Metadata.DateTimeFormat.DateOnly,
+                        ImeMode = ImeMode.Disabled
+                    };
+                    addedAttributes.Add(dtAttribute);
+                    break;
+                case "decimal":
+                    DecimalAttributeMetadata decimalAttribute = new DecimalAttributeMetadata()
+                    {                       
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        MaxValue = 100,
+                        MinValue = 0,
+                        Precision = 1
+                    };
+                    addedAttributes.Add(decimalAttribute);
+                    break;
+                case "int":
+                    IntegerAttributeMetadata integerAttribute = new IntegerAttributeMetadata()
+                    {
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        Format = IntegerFormat.None,
+                        MaxValue = 100,
+                        MinValue = 0
+                    };
+                    addedAttributes.Add(integerAttribute);
+                    break;
+                case "memo":
+                    MemoAttributeMetadata memoAttribute = new MemoAttributeMetadata()
+                    {
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        Format = StringFormat.TextArea,
+                        ImeMode = ImeMode.Disabled,
+                        MaxLength = 500
+                    };
+                    addedAttributes.Add(memoAttribute);
+                    break;
+                case "money":
+                    MoneyAttributeMetadata moneyAttribute = new MoneyAttributeMetadata()
+                    {
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        MaxValue = 1000.00,
+                        MinValue = 0.00,
+                        Precision = 1,
+                        PrecisionSource = 1,
+                        ImeMode = ImeMode.Disabled
+                    };
+                    addedAttributes.Add(moneyAttribute);
+                    break;
+                case "optionset":
+                    PicklistAttributeMetadata pickListAttribute =
+                     new PicklistAttributeMetadata()
+                     {
+                         SchemaName = $"{prefix}_{fieldUniqueName}",
+                         LogicalName = $"{prefix}_{fieldUniqueName}",
+                         DisplayName = new Label(fieldDisplayName, languageCode),
+                         RequiredLevel = fieldRequiredLevel,
+                         Description = new Label("This field is created by SolMate", languageCode),
+                         OptionSet = new OptionSetMetadata
+                         {
+                             IsGlobal = false,
+                             OptionSetType = OptionSetType.Picklist,
+                             Options =
+                              {
+                                 new OptionMetadata(
+                                    new Label("Option1", languageCode), null),
+                                 new OptionMetadata(
+                                    new Label("Option2", languageCode), null),
+                                 new OptionMetadata(
+                                    new Label("Option3", languageCode), null)
+                              }
+                         }
+                     };
+                    addedAttributes.Add(pickListAttribute);
+                    break;
+                case "string":                    
+                    StringAttributeMetadata stringAttribute = new StringAttributeMetadata
+                    {
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        MaxLength = 100
+                    };
+                    addedAttributes.Add(stringAttribute);
+                    break;
+                case "multiselect":
+                    MultiSelectPicklistAttributeMetadata multiSelectOptionSetAttribute = new MultiSelectPicklistAttributeMetadata()
+                    {
+                        SchemaName = $"{prefix}_{fieldUniqueName}",
+                        LogicalName = $"{prefix}_{fieldUniqueName}",
+                        DisplayName = new Label(fieldDisplayName, languageCode),
+                        RequiredLevel = fieldRequiredLevel,
+                        Description = new Label("This field is created by SolMate", languageCode),
+                        OptionSet = new OptionSetMetadata()
+                        {
+                            IsGlobal = false,
+                            OptionSetType = OptionSetType.Picklist,
+                            Options = {
+                            new OptionMetadata(new Label("First Option",languageCode),null),
+                            new OptionMetadata(new Label("Second Option",languageCode),null),
+                            new OptionMetadata(new Label("Third Option",languageCode),null)
+                            }
+                        }
+                    };
+                    addedAttributes.Add(multiSelectOptionSetAttribute);
+                    break;
+                default:
+                    break;
+                }
+          
+            foreach (AttributeMetadata columnDefinition in addedAttributes)
+            {                
+                CreateAttributeRequest request = new CreateAttributeRequest()
+                {
+                    EntityName = entityLogicalName,
+                    Attribute = columnDefinition,
+                    SolutionUniqueName = solutionUniqueName                    
+                };
+               
+                var response = (CreateAttributeResponse)service.Execute(request);                
+                tracingService.Trace($"Response: {response.AttributeId.ToString()}");
+                Response.Set(context, response.AttributeId.ToString());
+            }
+        }
+    }
+}
